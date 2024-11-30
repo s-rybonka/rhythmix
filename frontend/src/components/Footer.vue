97017383b56ea2409f6a12e38389207808974cd6
Stanislav Rybonka <rybonka@steelkiwi.com>
2024-11-30 17:07:08 +0200

    Add Footer component.

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container fluid class="bg-info text-white py-3 fixed-bottom">
+    <p class="mb-0 text-center">&copy; 2024 All Rights Reserved</p>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+body {
+  padding-bottom: 3rem;
+}
+</style>
